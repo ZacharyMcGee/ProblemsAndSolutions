2f843b28d0f6be3e43b6d979eab9172ce0b35c78
Zachary McGee <zachary.mcgee@siu.edu>
2020-03-01 13:44:17 -0600

    Create Staircase.cs

---
+++ HackerRank/Staircase/Staircase.cs
@@ -0,0 +1,34 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for(int i = 1; i <= n; i++) {
+            StringBuilder step = new StringBuilder(String.Empty);
+
+            step.Append(' ', n - i);
+            step.Append('#', i);
+
+            Console.WriteLine(step);
+        }
+    }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
